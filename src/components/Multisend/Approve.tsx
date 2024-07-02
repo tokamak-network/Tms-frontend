5319c58abf6bd86a35b7f614a72004ed6ac7c076
Shivansh <shivansh0301@gmail.com>
2024-07-02 14:09:22 +0530

    changed css

--- src/components/Multisend/Approve.tsx
+++ src/components/Multisend/Approve.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { useBalance, useAccount } from 'wagmi';
 import { ethers } from 'ethers';
 
-const StatCard = ({ title, value, bgColor = 'bg-indigo-950', textColor = 'text-white' }) => (
-  <div className={`h-20 w-80 border border-gray-500 ${bgColor} rounded-md flex flex-col items-center justify-center`}>
+const StatCard = ({ title, value, bgColor = '', textColor = 'text-black' }) => (
+  <div
+    className={`h-20 w-80 border border-gray-500 ${bgColor} rounded-md flex flex-col items-center justify-center`}
+  >
     <h1 className={`font-bold text-3xl ${textColor}`}>{value}</h1>
-    <h1 className='text-indigo-600 text-base'>{title}</h1>
+    <h1 className='text-base'>{title}</h1>
   </div>
 );
 
 const ApproveComponent = ({ tokenBalance, tokenDetails, recipients }) => {
   const account = useAccount().address;
   const ethBalance = useBalance({ address: account }).data?.value;
-  const ethBalanceFormatted = parseFloat(ethers.formatEther(BigInt(ethBalance))).toFixed(3);
+  const ethBalanceFormatted = parseFloat(
+    ethers.formatEther(BigInt(ethBalance))
+  ).toFixed(3);
 
   const symbol = tokenDetails.symbol;
   const allowance = parseInt(tokenDetails.allowance);
@@ -29,24 +33,37 @@ const ApproveComponent = ({ tokenBalance, tokenDetails, recipients }) => {
         })
         .filter((recipient) => recipient !== null)
     : [];
-  const totalTokensToSend = parsedRecipients.reduce((acc, current) => acc + current.amount, 0);
+  const totalTokensToSend = parsedRecipients.reduce(
+    (acc, current) => acc + current.amount,
+    0
+  );
 
   return (
-    <div className='flex flex-col px-5 py-6 mt-5 max-w-65 rounded shadow-sm bg-indigo-950 w-full font-sans'>
-      <div className='h-full w-full bg-indigo-950 border border-indigo-950 rounded-md shadow-md'>
-        <div className='h-full w-full border border-gray-500 bg-indigo-950 rounded-md mx-auto my-10 p-5'>
+    <div className='flex flex-col px-5 py-6 mt-5 max-w-65 rounded shadow-sm  w-full '>
+      <div className='h-full w-full  border border-grey-950 rounded-md '>
+        <div className='h-full w-full border border-gray-500  rounded-md mx-auto my-10 p-5'>
           <div className='flex justify-center gap-5 mt-5'>
-            <StatCard title='Total number of tokens to send' value={totalTokensToSend.toFixed(3)} />
-            {tokenDetails && <StatCard title='Your token balance' value={tokenBalance} />}
+            <StatCard
+              title='Total number of tokens to send'
+              value={totalTokensToSend.toFixed(3)}
+            />
+            {tokenDetails && (
+              <StatCard title='Your token balance' value={tokenBalance} />
+            )}
             <StatCard title='Your ETH balance' value={ethBalanceFormatted} />
           </div>
-          <div className='bg-indigo-950 mt-5'>
-            <h1 className='text-white text-xl font-bold p-3'>Lists of recipients</h1>
+          <div className=' mt-5 ml-2'>
+            <h1 className='text-black text-xl font-bold p-3 flex justify-between'>
+              Addresses List
+              <span className='text-blue text-xl'>Amounts</span>
+            </h1>
             {parsedRecipients.length > 0 ? (
               parsedRecipients.map((recipient, index) => (
                 <div key={index} className='flex justify-between p-3'>
-                  <h1 className='text-indigo-800 text-xl'>{recipient.address}</h1>
-                  <h1 className='text-white text-xl'>{recipient.amount} {symbol}</h1>
+                  <h1 className='text-blue-800 text-xl'>{recipient.address}</h1>
+                  <h1 className='text-blue text-xl'>
+                    {recipient.amount} {symbol ? symbol : 'TON'}
+                  </h1>
                 </div>
               ))
             ) : (
@@ -54,15 +71,17 @@ const ApproveComponent = ({ tokenBalance, tokenDetails, recipients }) => {
             )}
           </div>
           <div className='mt-10'>
-            <h1 className='text-xl font-bold text-white ml-3'>Amount to approve</h1>
+            <h1 className='text-xl font-bold text-white ml-3'>
+              Amount to approve
+            </h1>
             <div className='flex flex-row gap-4 mt-5 ml-4'>
               <input type='radio' id='exact-amount' name='amount-type' />
-              <label htmlFor='exact-amount' className='text-white text-xl mr-8'>
-                Exact amount to be sent (2.049 UNO)
+              <label htmlFor='exact-amount' className='text-grey text-xl mr-8'>
+                Approve Exact Amount
               </label>
               <input type='radio' id='unlimited-amount' name='amount-type' />
-              <label htmlFor='unlimited-amount' className='text-white text-xl'>
-                Unlimited amount
+              <label htmlFor='unlimited-amount' className='text-grey text-xl'>
+                Approve Unlimited Amount
               </label>
             </div>
           </div>
