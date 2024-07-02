2d7a30ef18a699573025d933714a156a73ea9735
Shivansh <shivansh0301@gmail.com>
2024-07-02 13:44:37 +0530

    changed css

--- src/components/Multisend/Prepare.tsx
+++ src/components/Multisend/Prepare.tsx
@@ -21,11 +21,10 @@ const PrepareComponent = ({ setTokenDetails, setCSVData }) => {
   };
 
   return (
-    <div className='flex flex-col px-5 py-6 mt-5 max-w-full rounded shadow-sm bg-indigo-950 w-[566px] font-sans'>
+    <div className='flex flex-col px-5 py-6 mt-5 max-w-full rounded shadow-sm bg-white-950 w-[566px] font-sans'>
       <div className='flex gap-px max-md:flex-wrap'>
         <div className='flex flex-col grow shrink-0 my-auto text-xs basis-0 text-neutral-300 w-fit max-md:max-w-full'>
-          <div className='leading-[164%] max-md:max-w-full'>Token Address</div>
-          <div className='flex gap-1.5 px-3.5 py-2.5 mt-2 text-indigo-500 rounded border border-solid bg-indigo-950 border-zinc-400 leading-[155%] max-md:flex-wrap'>
+          <div className='flex gap-1.5 px-3.5 py-2.5 mt-2 text-indigo-500 rounded border border-solid border-zinc-400 leading-[1%] max-md:flex-wrap box-border w-full h-full rounded-full border-1 border-solid border-gray-300'>
             <img
               loading='lazy'
               src='https://cdn.builder.io/api/v1/image/assets/TEMP/4792370c216477454484b83d83d4e4aaae409419cf9974d042cb7489c6fefe2e?'
@@ -34,12 +33,14 @@ const PrepareComponent = ({ setTokenDetails, setCSVData }) => {
             />
             <input
               type='text'
-              placeholder='Enter your Token Address'
-              className='flex-auto my-auto bg-transparent text-indigo-500 placeholder-indigo-500 focus:outline-none'
+              placeholder='Search Token name or address'
+              className='flex-auto my-auto bg-transparent text-gray-600 placeholder-gray-600 focus:outline-none
+               font-poppins font-normal text-base leading-[21px] text-cap leading-trim-both '
               value={tokenAddress}
               onChange={handleInputChange}
             />
           </div>
+
           {error && <div className='mt-2 text-xs text-red-500'>{error}</div>}
           {tokenAddress && !error && (
             <TokenDetails
