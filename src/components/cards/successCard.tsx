fd8dbbb01f155b46e0f9794f82836d1168442456
Shivansh <shivansh0301@gmail.com>
2024-07-03 12:25:32 +0530

    font add

--- src/components/cards/successCard.tsx
+++ src/components/cards/successCard.tsx
@@ -17,8 +17,7 @@ const SuccessCard = ({ txnHash }) => {
   return (
     <div className='flex flex-col px-5 py-6 mt-5 max-w-65 rounded w-full '>
       <div className='h-full w-full  rounded-md mx-auto my-10 p-5 text-center '>
-        <h1 className=' text-xl font-bold p-3 flex justify-center'>SUCCESS!</h1>
-
+        <h1 className=' text-xl font-poppins text-bold  p-3 flex justify-center'>SUCCESS!</h1>
         <p className='text-base mb-4 '>
           View transaction on{' '}
           <a
