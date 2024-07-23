dc697b6d6ca3225defccd75599ce1d324681ef17
Shivansh <shivansh0301@gmail.com>
2024-07-23 11:28:28 +0530

    add search query

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -4,13 +4,16 @@ const contracts = {
     55004:'0x2f267b4646cfdB26b18c4571B5c41E8b415Ab9aA'
   },
   usdc: {
-    55007: ''
+    55007: '0xFF3Ef745D9878AfE5934Ff0b130868AFDDbc58e8'
   },
   usdt: {
+    55007: '0x79E0d92670106c85E9067b56B8F674340dCa0Bbd'
+  },
+  wton: {
     55007: ''
   },
   ton: {
-    55007: ''
+    55007: '0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2'
   }
 };
 
--- src/hooks/getCurrentNetwork.tsx
+++ src/hooks/getCurrentNetwork.tsx
@@ -1,9 +1,8 @@
 import { getAccount, getEnsName, getClient } from '@wagmi/core';
 import { NetworkConfig } from '../config/networks';
 
-const getCurrentNetwork = async () => {
+const getCurrentNetwork = () => {
   const { chain } = getClient(NetworkConfig);
-
   return {
     chain
   };
