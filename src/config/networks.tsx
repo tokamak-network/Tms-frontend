bbadc05cb7e42fe20cc45b75042723099002e2d8
Shivansh <shivansh0301@gmail.com>
2024-09-25 12:04:22 +0530

    removed rainboq wallet

--- src/config/networks.tsx
+++ src/config/networks.tsx
@@ -1,12 +1,31 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia_titan ,titan} from '../chains/tokamak';
-export const NetworkConfig = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    //titan,
-    sepolia_titan,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia_titan] : [])
+import { sepolia_titan, titan } from '../chains/tokamak';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { createConfig, http } from 'wagmi';
+import { createClient } from 'viem';
+import {
+  metaMaskWallet,
+  braveWallet,
+  coinbaseWallet,
+  walletConnectWallet
+} from '@rainbow-me/rainbowkit/wallets';
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Popular',
+      wallets: [metaMaskWallet, braveWallet, coinbaseWallet, walletConnectWallet]
+    }
   ],
+  {
+    appName: 'Tokamak MultiSender',
+    projectId: 'YOUR_PROJECT_ID'
+  }
+);
+export const NetworkConfig = createConfig({
+  chains: [titan, sepolia_titan],
+  client({ chain }) {
+    return createClient({ chain, transport: http() });
+  },
+  connectors,
   ssr: true
 });
